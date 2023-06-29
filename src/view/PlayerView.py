f2f0a45c23992331372822dd30cf8cd992d3a11c
Ádám Székely <szekelyadam@edu.bme.hu>
2023-06-30 01:53:18 +0200

    Controller partially implemented

--- src/view/PlayerView.py
+++ src/view/PlayerView.py
@@ -1,42 +1,18 @@
 # coding:utf-8
 from PyQt5.QtWidgets import QFrame, QVBoxLayout
+from PyQt5.QtCore import QObject, pyqtSignal
 from PyQt5.QtGui import QIcon
 from src.model import MediaData, Player
 from util.CustomControls import PlayerPanel,VolumePanel, AlbumPanel, MetaTablePanel
 import pythoncom
 
 class PlayerView(QFrame):
-    def __init__(self, text: str, parent=None):
+
+    def __init__(self, controller, parent=None):
         super().__init__(parent= parent)
+        self.controller = controller
 
-        self.audio = MediaData.MediaData('D:/Zene/test/Airdrop - tom.mp3')
-        #print(self.audio.artist)
-        #print(self.audio.length)
-        #print(self.audio.type)
-
-        self.mediaPlayer = Player.Player('D:/Zene/test/Airdrop - tom.mp3')
-        self.mediaPlayer.positionChanged.connect(self.positionChanged)
-        self.mediaPlayer.mediaChanged.connect(self.mediaChanged)
-        self.mediaPlayer.mediaEnd.connect(self.mediaEnd)
-
-        self.playerPanel = PlayerPanel()
-        self.volumePanel = VolumePanel()
-        self.albumPanel = AlbumPanel(self.audio)
-        self.metaPanel = MetaTablePanel(self.audio)
-
-        # Vertical Layout
-        vboxLayout = QVBoxLayout()
-        vboxLayout.setContentsMargins(10,0,10,0)
-        vboxLayout.addWidget(self.albumPanel)
-        vboxLayout.addWidget(self.metaPanel)
-        vboxLayout.addWidget(self.volumePanel)
-        vboxLayout.addWidget(self.playerPanel)
-        vboxLayout.setStretchFactor(self.albumPanel, 0)
-        vboxLayout.setStretchFactor(self.playerPanel, 0)
-        vboxLayout.setStretchFactor(self.volumePanel,0)
-        self.setLayout(vboxLayout)
-
-        # Connecting button actions
+        self.initUi()
         self.playerPanel.playButton.clicked.connect(self.play)
         self.playerPanel.leftButton.clicked.connect(self.left)
         self.playerPanel.rightButton.clicked.connect(self.right)
@@ -47,36 +23,32 @@ class PlayerView(QFrame):
         self.volumePanel.volumeSlider.valueChanged.connect(self.setVolume)
 
     def play(self):
-        if self.mediaPlayer.player.is_playing():
-            self.mediaPlayer.player.pause()
+        if self.controller._isPlaying():
+            self.controller._stopPlaying()
             self.playerPanel.playButton.setIcon(QIcon('icons/play.svg'))
         else:
-            self.mediaPlayer.player.play()
+            self.controller._startPlaying()
             self.playerPanel.playButton.setIcon(QIcon('icons/pause.svg'))
 
     def left(self):
-        self.mediaPlayer.player.stop()
         #TODO
+        pass
 
     def right(self):
-        self.mediaPlayer.player.stop()
         #TODO
+        pass
 
     def setPosition(self, position):
-       # Update the slider
-        self.mediaPlayer.player.set_position(float(position / 100)) # VLC uses a 0.0-1.0 range
 
-        # Update the time
-        current_time = self.mediaPlayer.player.get_time() / 1000   # Current time in seconds
-        length = self.mediaPlayer.player.get_length() / 1000    # Length in seconds
+        self.controller._setPosition(float(position / 100)) # VLC uses a 0.0-1.0 range
+        current_time = self.controller._requestPlayerTime() / 1000   # Current time in seconds
         self.playerPanel.currentTime.setText(self.convert_seconds(int(current_time))) 
        
-    def positionChanged(self, position):
-        # Current time
-        current_time = self.mediaPlayer.player.get_time() / 1000   # Current time in seconds
-        length = self.mediaPlayer.player.get_length() / 1000    # Length in seconds
+    def positionChanged(self, current_time, length):
         self.playerPanel.currentTime.setText(self.convert_seconds(int(current_time))) 
 
+        # Track time
+        self.playerPanel.trackTime.setText(self.convert_seconds(length / 1000))
         # Update time slider 
         percentage_played = (current_time / length) * 100
         self.playerPanel.slider.setValue(int(percentage_played))
@@ -88,27 +60,22 @@ class PlayerView(QFrame):
         
     def mediaChanged(self):
         #TODO
-        self.mediaPlayer.player.set_position(0.0)
-        self.playerPanel.trackTime.setText(self.audio.length)
-        self.playerPanel.playButton.setIcon(QIcon('icons/pause.svg'))
-        self.mediaPlayer.player.audio_set_volume(50)
-        
-        self.mediaPlayer.player.play()
-        
+        pass
+
     def mute(self):
-        self.mediaPlayer.player.audio_set_mute(True)
+        self.controller._muteAudio(True)
         self.volumePanel.volumeSlider.setSliderPosition(0)
 
     def unMute(self):
-        self.mediaPlayer.player.audio_set_mute(False)
+        self.controller._muteAudio(False)
         if self.volumePanel.volumeSlider.value() <= 90: self.volumePanel.volumeSlider.setValue(self.volumePanel.volumeSlider.value() + 10)
         
     def setVolume(self, volume):
         icon_path = 'icons/volume2.svg' if volume > 50 else 'icons/volume1.svg' if volume > 20 else 'icons/volume0.svg'
         self.volumePanel.unmuteButton.setIcon(QIcon(icon_path))
-        self.mediaPlayer.player.audio_set_mute(False)
-        self.mediaPlayer.player.audio_set_volume(volume) # volume is in the range 0-100
-        print(volume)
+
+        self.controller._muteAudio(False)
+        self.controller._setVolume(volume)
 
     def mediaEnd(self):
         self.playerPanel.currentTime.setText("0:00")
@@ -119,5 +86,25 @@ class PlayerView(QFrame):
         self.playerPanel.rightButton.setDisabled(True)
         self.playerPanel.playButton.setDisabled(True)
 
-   
+    def _tryPopulate(self):
+        pass
+    
+    def setController(self, controller):
+        self.controller = controller
+    
+    def initUi(self):
+        self.playerPanel = PlayerPanel()
+        self.volumePanel = VolumePanel()
+        self.albumPanel = AlbumPanel(self.controller._requestAudio())
+        self.metaPanel = MetaTablePanel(self.controller._requestAudio())
+        vboxLayout = QVBoxLayout()
+        vboxLayout.setContentsMargins(10,0,10,0)
+        vboxLayout.addWidget(self.albumPanel)
+        vboxLayout.addWidget(self.metaPanel)
+        vboxLayout.addWidget(self.volumePanel)
+        vboxLayout.addWidget(self.playerPanel)
+        vboxLayout.setStretchFactor(self.albumPanel, 0)
+        vboxLayout.setStretchFactor(self.playerPanel, 0)
+        vboxLayout.setStretchFactor(self.volumePanel,0)
+        self.setLayout(vboxLayout)
         
